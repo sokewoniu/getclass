3b36501ca43e98e03607c2a1961f7175559e03b3
liaw <liaw@china-coast.com>
2024-07-26 16:03:20 +0800

    test formdata

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -1,8 +1,9 @@
 export async function onRequest(context) {
     const { d1 } = context.env;
-    const stmt = d1.prepare("SELECT * FROM student LIMIT 3");
-    const { results } = await stmt.all();
-    let pretty = JSON.stringify({ok:true,data:results,req:JSON.parse(context.request.text())}, null, 2);
+   const params = context.request.formData();
+    const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(params.get('name'),params.get('code'));
+    const data = await stmt.first();
+    let pretty = JSON.stringify({ok:true,data}, null, 2);
     return new Response(pretty, {
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
