2bc0ef0347a3e3beb577740fa4580e39e1b6e69f
liaw <liaw@china-coast.com>
2024-07-26 10:21:30 +0800

    add status

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,3 @@
 export async function onRequest(context) {
-    const { d1 } = env;
-    const stmt = d1.prepare("SELECT * FROM student LIMIT 3");
-    const { results } = await stmt.all();
     return  new Response("It's a Test");
 }
