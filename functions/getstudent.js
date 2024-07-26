11915bc6c7cb962b7122bf64910b8f432edcf07d
liaw <liaw@china-coast.com>
2024-07-26 11:36:42 +0800

    modif json

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -2,5 +2,10 @@ export async function onRequest(context) {
     const { d1 } = env;
     const stmt = d1.prepare("SELECT * FROM student LIMIT 3");
     const { results } = await stmt.all();
-    return  Response.json({ok:true,data:results});
+    let pretty = JSON.stringify({ok:true,data:results}, null, 2);
+    return new Response(pretty, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
 }
