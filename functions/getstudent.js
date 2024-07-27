2d738809f73267a57b82ec955670d827377267da
liaw <liaw@china-coast.com>
2024-07-27 11:49:32 +0800

    await formData

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-    /*const { d1 } = context.env;
+    const { d1 } = context.env;
     const params = await context.request.formData();
     const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(params.get('name'),params.get('code'));
     const data = await stmt.first();
