da5277a3d7629c2754dc6252fa10fb1de4a7de80
liaw <liaw@china-coast.com>
2024-07-27 14:32:16 +0800

    test Json

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -1,8 +1,8 @@
 export async function onRequest(context) {
 try{
     const { d1 } = context.env;
-    const params = await context.request.formData();
-    const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(params.get('name'),params.get('code'));
+    const json = await context.request.json();
+    const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(json.name,json.code);
     const data = await stmt.first();
     let pretty = JSON.stringify({ok:true,data,}, null, 2);
     return new Response(pretty, {
