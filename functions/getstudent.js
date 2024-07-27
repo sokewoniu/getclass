b77884d2b03bcdc4349cc5dd4108d04ea04f1846
liaw <liaw@china-coast.com>
2024-07-27 15:37:03 +0800

    rm test.js

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -1,25 +1,13 @@
 export async function onRequest(context) {
-try{
-    const { d1 } = context.env;
-    const json = await context.request.json();
-    const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(json.name,json.code);
-    const data = await stmt.first();
-    let pretty = JSON.stringify({ok:true,data,}, null, 2);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
-}
-catch(e)
-{
-let pretty = JSON.stringify({error:e.message}, null, 2);
-
-return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
-
-}
+  try{
+      const { d1 } = context.env;
+      const json = await context.request.json();
+      const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(json.name,json.code);
+      const data = await stmt.first();
+      return Response.json(data)
+  }
+  catch(e)
+  {
+    return Response.json({error:e.message});
+  }
 }
