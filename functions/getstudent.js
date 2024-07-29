3d4a5cd90a5348ec0c054e70732b93a8c7a49483
liaw <liaw@china-coast.com>
2024-07-29 15:25:16 +0800

    add ok

--- functions/getstudent.js
+++ functions/getstudent.js
@@ -2,9 +2,9 @@ export async function onRequest(context) {
   try{
       const { d1 } = context.env;
       const json = await context.request.json();
-      const stmt = d1.prepare("SELECT * FROM student where name=?1 and code=?2").bind(json.name,json.code);
+      const stmt = d1.prepare("SELECT * FROM student where name=? and code=?").bind(json.name,json.code);
       const data = await stmt.first();
-      return Response.json(data)
+      return Response.json({ok:true,data})
   }
   catch(e)
   {
